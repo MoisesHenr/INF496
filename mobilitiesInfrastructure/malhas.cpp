e9b3fc04909d0fcbee523630a9f3e2be002ca556
MoisesHenr <38496675+MoisesHenr@users.noreply.github.com>
2019-05-11 11:09:15 -0300

    automatização1.0

---
+++ mobilitiesInfrastructure/malhas.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>
+#include <algorithm>
+#include <fstream>
+
+using namespace std;
+
+struct Corner{
+    float u; //up
+    float d; //down
+    float l; //left
+    float r; //right
+};
+
+Corner block[4][6];
+ofstream out;
+
+//arrumar...
+void Way (int posI, int posJ, int posIb, int posJb, int &depth) {
+    depth++;
+
+    if (depth >= 50) {
+        return;
+    }
+
+    int u, d, l, r, m;
+
+    u = (rand() % 10 + 1)*block[posI][posJ].u;
+    d = (rand() % 10 + 1)*block[posI][posJ].d;
+    l = (rand() % 10 + 1)*block[posI][posJ].l;
+    r = (rand() % 10 + 1)*block[posI][posJ].r;
+
+    m = max(u, max(d, max(l, r)));
+
+    if (m == u && posI > 0 && posI-1 != posIb) {
+        out << "    <moveto x=\"" << posJ*100+20 << "\" y=\"" << (posI-1)*100+10 << "\"/>" << endl;
+        Way(posI-1, posJ, posI, posJ, depth);
+    }
+    else if (m == d && posI < 3 && posI+1 != posIb) {
+        out << "    <moveto x=\"" << posJ*100+20 << "\" y=\"" << (posI+1)*100+10 << "\"/>" << endl;
+        Way(posI+1, posJ, posI, posJ, depth);
+    }
+    else if (m == l && posJ > 0 && posJ-1 != posJb) {
+        out << "    <moveto x=\"" << (posJ-1)*100+20 << "\" y=\"" << posI*100+10 << "\"/>" << endl;
+        Way(posI, posJ-1, posI, posJ, depth);
+    }
+    else if (m == r && posJ < 5 && posJ+1 != posJb) {
+        out << "    <moveto x=\"" << (posJ+1)*100+20 << "\" y=\"" << posI*100+10 << "\"/>" << endl;
+        Way(posI, posJ+1, posI, posJ, depth);
+    }
+    else {
+        Way(posI, posJ, posIb, posJb, depth);
+    }
+}
+
+int main() {
+    int posI, posJ, depth = -1;
+    int numRoutes;
+
+    cout << "Entre com o numero de rotas desejadas: ";
+    cin >> numRoutes;
+
+    for (int j = 0; j < 6; j++) { //line 0 and 2 --- left
+        block[0][j].l = 1;
+        block[1][j].l = 0;
+        block[2][j].l = 2;
+        block[3][j].l = 0;
+    }
+
+    for (int j = 0; j < 6; j++) { //line 1, 2 and 3 --- right
+        block[0][j].r = 0;
+        block[1][j].r = 1;
+        block[2][j].r = 2;
+        block[3][j].r = 1;
+    }
+
+    for (int i = 0; i < 4; i++) { //row 0, 2 and 4 --- down
+        block[i][0].d = 1;
+        block[i][1].d = 0;
+        block[i][2].d = 2;
+        block[i][3].d = 0;
+        block[i][4].d = 1;
+        block[i][5].d = 0;
+    }
+
+    for (int i = 0; i < 4; i++) { //row 1, 2, 3 and 5 --- up
+        block[i][0].u = 0;
+        block[i][1].u = 1;
+        block[i][2].u = 2;
+        block[i][3].u = 1;
+        block[i][4].u = 0;
+        block[i][5].u = 1;
+    }
+
+    srand (time(NULL));
+    for (int iter = 0; iter < numRoutes; iter++) {
+        posI = rand() % 4;
+        posJ = rand() % 6;
+
+        string name;
+        name = "routes/" + to_string(iter) + ".xml";
+
+        out.open(name);
+        out << "<movement>" << endl;
+        out << "    <set speed=\"10\" x=\"" << posJ*100+20 << "\" y=\"" << posI*100+10 << "\"/>" << endl;
+        Way(posI, posJ, posI, posJ, depth);
+        depth = 0;
+        out << "</movement>";
+        out.close();
+    }
+    
+    cout << "Geração de rotas terminada" << endl;
+
+    return 0;
+}
---
+++ mobilitiesInfrastructure/run.py
@@ -0,0 +1,100 @@
+import os
+import re
+
+# gerar malhas
+r = input("Deseja gerar novas rotas (s/n): ")
+
+if r == "s" or r == "S":
+	os.system("./malhas")
+
+# atualizar .ini
+a = input("Deseja atualizar *.ini (s/n): ")
+
+if a == "s" or a == "S":
+	ini = open("omnetpp.ini", "r")
+
+	numRoutes = int(input("Informe o numero de rotas existentes: "))
+	temp = ""
+
+	for linha in ini:
+		aux = re.split("[\. ]", linha) #split usando expressao regular para "." e " "
+		if "turtleScript" in aux: #fazer esse passo em um for para que tenha xmldoc() o suficiente para cada malha
+			temp += '*.host1.mobility.turtleScript = ${count='
+			for i in range(numRoutes):
+				if (i < numRoutes - 1):
+					temp += 'xmldoc("routes/' + str(i) + '.xml"),'
+				else:
+					temp += 'xmldoc("routes/' + str(i) + '.xml")'
+			temp += '}\n'
+		else:
+			temp += linha
+
+	ini.close()
+
+	#print(temp)
+
+	ini = open("omnetpp.ini", "w")
+	ini.writelines(temp)
+	ini.close()
+
+	print("Atualização do *.ini terminada")
+
+# rodar simulação
+s = input("Deseja rodar as simulações (s/n): ")
+
+if s == "s" or s == "S":
+	numS = int(input("Rodar simulações referentes a quantas malhas: "))
+
+	for i in range(numS):
+		# identificação do tempo limite de cada malha
+		xml = open("routes/" + str(i) + ".xml", "r")
+		linhas = xml.readlines()
+		timeLimit = (len(linhas) - 3) * 10
+
+		print("timeLimit: %d", timeLimit)
+
+		# comando para simulação
+		os.system("opp_run -m -n ../../../src:../../../examples:../../../tutorials:../.. --image-path=../../../images -l ../../../src/INET omnetpp.ini -u Cmdenv -c Infrastructure -r " + str(i) + " --sim-time-limit=" + str(timeLimit) + "s")
+
+		# mudar o nome de saida para saida_i onde i é o indice
+		os.rename("/home/moises/inet4/showcases/wireless/mobilitiesInfrastructure/results2IA/posTime.txt", "/home/moises/inet4/showcases/wireless/mobilitiesInfrastructure/results2IA/posTime_" + str(i) + ".txt")
+
+		# passar script para "limpar" o arquivo *.tlog
+		tlog = open("results/Infrastructure-" + str(i) + ".tlog", "r")
+		temp = open("results2IA/assocResp_probeReq-" + str(i) + ".txt", "w")
+
+		for linha in tlog:
+			aux = linha.split()
+			if "AssocResp-OK" in aux:
+				if "InfrastructureShowcaseA.host1.wlan[0].radio" in aux:
+					temp.writelines(linha)
+			if "ProbeReq" in aux:
+				if "InfrastructureShowcaseA.host1.wlan[0].radio" in aux:
+					temp.writelines(linha)
+
+		tlog.close()
+		temp.close()
+
+		# resumo dos probReq
+		temp = open("results2IA/assocResp_probeReq-" + str(i) + ".txt", "r")
+		tlog_r = open("results2IA/assocResp_probeReq-" + str(i) + "_r.txt", "w")
+
+		linhas = temp.readlines()
+
+		tlog_r.writelines(linhas[0])
+
+		for i in range(1,len(linhas) - 1):
+			aux1 = linhas[i-1]
+			aux1 = aux1.split()
+			aux2 = linhas[i]
+			aux2 = aux2.split()
+			aux3 = linhas[i+1]
+
+			if "ProbeReq" in aux1 and "ProbeReq" in aux2 and "ProbeReq" in aux3:
+				pass
+			else:
+				tlog_r.writelines(linhas[i])
+
+		tlog_r.writelines(linhas[len(linhas) - 1])
+
+	print("Simulações terminadas")
